384992d894b830456dec9882a81cf59fb37705ab
takes12no1 <takes12no1@gmail.com>
2018-03-01 12:18:32 -0600

    Documentation configuration complete

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import HelloWorld from 'saaspress-react-components/HelloWorld';
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+				return <HelloWorld message="SaaSPress Users" />
+}
